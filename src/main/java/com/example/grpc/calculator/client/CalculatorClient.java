e780d0966ef50ff746e334e8fcea24c3307f3b6a
ajay murtekar <ajaymurtekar@microsoft.com>
2025-08-08 10:04:53 +0530

    Added examples for client streaming

--- src/main/java/com/example/grpc/calculator/client/CalculatorClient.java
+++ src/main/java/com/example/grpc/calculator/client/CalculatorClient.java
@@ -3,31 +3,78 @@ package com.example.grpc.calculator.client;
 import com.proto.calculator.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class CalculatorClient {
     public static void main(String[] args) {
+        CalculatorClient client = new CalculatorClient();
+        client.run();
+    }
+
+    private void run() {
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
                 .usePlaintext()
                 .build();
 
-        CalculatorServiceGrpc.CalculatorServiceBlockingStub calculatorService = CalculatorServiceGrpc.newBlockingStub(channel);
-
-        //Unary operation
-//        SumRequest request = SumRequest.newBuilder().setFirstNumber(5).setSecondNumber(7).build();
-//
-//        SumResponse response = calculatorService.sum(request);
-//
-//        System.out.println("Addition result: "+ response.getResult());
-
-        //Server Streaming
-        calculatorService.primeNumberDecomposition(PrimeNumberDecompositionRequest.newBuilder().setNumber(567890)
-                        .build())
-                        .forEachRemaining(primeNumberDecompositionResponse -> {
-                            System.out.println(primeNumberDecompositionResponse.getPrimeFactor());
-                        });
-
-
+        //doUnaryCall(channel);
+        //doServerStreamingCall(channel);
+        doClientStreamingCall(channel);
 
         channel.shutdown();
+
+    }
+
+    private void doClientStreamingCall(ManagedChannel channel) {
+        CalculatorServiceGrpc.CalculatorServiceStub calculatorService = CalculatorServiceGrpc.newStub(channel);
+        CountDownLatch latch = new CountDownLatch(1);
+        StreamObserver<ComputeAverageRequest> requestObserver = calculatorService.computeAverage(new StreamObserver<ComputeAverageResponse>() {
+
+            @Override
+            public void onNext(ComputeAverageResponse computeAverageResponse) {
+                System.out.println("Received response from server"+computeAverageResponse.getAverage());
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("Server completed sending data");
+                latch.countDown();
+            }
+        });
+        requestObserver.onNext(ComputeAverageRequest.newBuilder().setNumber(1).build());
+        requestObserver.onNext(ComputeAverageRequest.newBuilder().setNumber(2).build());
+        requestObserver.onNext(ComputeAverageRequest.newBuilder().setNumber(3).build());
+        requestObserver.onNext(ComputeAverageRequest.newBuilder().setNumber(4).build());
+
+        requestObserver.onCompleted();
+        try {
+            latch.await(3, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void doServerStreamingCall(ManagedChannel channel) {
+        CalculatorServiceGrpc.CalculatorServiceBlockingStub calculatorService = CalculatorServiceGrpc.newBlockingStub(channel);
+        calculatorService.primeNumberDecomposition(PrimeNumberDecompositionRequest.newBuilder().setNumber(567890)
+                        .build())
+                .forEachRemaining(primeNumberDecompositionResponse -> {
+                    System.out.println(primeNumberDecompositionResponse.getPrimeFactor());
+                });
+
+    }
+
+    private void doUnaryCall(ManagedChannel channel) {
+        CalculatorServiceGrpc.CalculatorServiceBlockingStub calculatorService = CalculatorServiceGrpc.newBlockingStub(channel);
+        SumRequest request = SumRequest.newBuilder().setFirstNumber(5).setSecondNumber(7).build();
+        SumResponse response = calculatorService.sum(request);
+        System.out.println("Addition result: "+ response.getResult());
     }
 }
--- src/main/java/com/example/grpc/calculator/server/CalculatorServiceImpl.java
+++ src/main/java/com/example/grpc/calculator/server/CalculatorServiceImpl.java
@@ -39,4 +39,32 @@ public class CalculatorServiceImpl extends CalculatorServiceGrpc.CalculatorServi
         }
         responseObserver.onCompleted();
     }
+
+    @Override
+    public StreamObserver<ComputeAverageRequest> computeAverage(StreamObserver<ComputeAverageResponse> responseObserver) {
+        StreamObserver<ComputeAverageRequest> requestObserver = new StreamObserver<ComputeAverageRequest>() {
+            int sum = 0;
+            int count = 0;
+
+            @Override
+            public void onNext(ComputeAverageRequest computeAverageRequest) {
+                sum = sum + computeAverageRequest.getNumber();
+                count++;
+
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                double average = (double) sum /count;
+                responseObserver.onNext(ComputeAverageResponse.newBuilder().setAverage(average).build());
+                responseObserver.onCompleted();
+            }
+        };
+        return requestObserver;
+    }
 }
